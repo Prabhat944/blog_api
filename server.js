dd8d2c41d2dee1097c77e82738ef3e9713bdd7eb
Prabhat kumar <prabhatkumar944@gmail.com>
2024-05-27 08:24:27 +0530

    user auth added for blog

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require('./config/db');
+
+dotenv.config();
+
+const app = express();
+
+connectDB();
+
+//Init middleware
+app.use(express.json({extended:false}));
+console.log('i m her')
+//Routes
+app.use("/api/auth/a",(req,res)=>{
+    res.status(200).json({msg:'yeah'})
+})
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/posts", require("./routes/posts"));
+// app.use("/api/comments", require("./routes/comments"));
+
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`server is running on port ${PORT}`);
+})
+
